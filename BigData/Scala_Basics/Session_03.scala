e2c5516e9f2d9de545e955f15f57fe14900d1b09
Luis Enrrique Morales Jauregui <enrrique.morales@uabc.edu.mx>
2025-02-26 21:12:55 -0800

    Session_03.scala

--- BigData/Scala_Basics/Session_03.scala
+++ BigData/Scala_Basics/Session_03.scala
@@ -1,3 +1,5 @@
+//Enrrique Jauregui
+
 **OPERACIONES MATEMATICAS**
 //Exponencia, en este ejemplo el cuatro se eleva al cuadrado, como resultado 16.
 math.pow(4, 2)  //Double = 16.0
@@ -205,73 +207,25 @@ mutmap.keys
 // Devuelve un iterable con los valores del mapa: Iterable(123, 5, 7, 777)
 mutmap.values
 
-1. Crea una lista llamada "lista" con los elementos "rojo", "blanco", "negro"
+//Practice 2
+//1. Crea una lista llamada "lista" con los elementos "rojo", "blanco", "negro"
 val lista = List("rojo", "blanco", "negro")
 
-2. Añadir 5 elementos mas a "lista" "verde" ,"amarillo", "azul", "naranja", "perla"
+//Terminal
+List(rojo, blanco, negro)
+
+//2. Añadir 5 elementos mas a "lista" "verde" ,"amarillo", "azul", "naranja", "perla"
 val nuevosElementos = List("verde", "amarillo", "azul", "naranja", "perla")
 val listaActualizada = lista ++ nuevosElementos
 
-3. Traer los elementos de "lista" "verde", "amarillo", "azul"
+//Terminal
+List(rojo, blanco, negro, verde, amarillo, azul, naranja, perla)
+
+//3. Traer los elementos de "lista" "verde", "amarillo", "azul"
 val sublista = listaActualizada.slice(3, 6)
 
-
-// Arrays
-// Arrays are mutable, List are not
-val arr = Array(3,4,5)
-val arr = Array("a","b","c")
-val arr = Array("a","b", true, 1.2)
-
-//Create arrays  with range method
-Array.range(0, 10)
-Array.range(0, 10, 2)
-
-Range(0,5)
-
-//Sets not cotains duplicate elements
-val s = Set()
-val s = Set(1,2,3)
-
-val s = Set(2,2,2,3,3,3,5,5,5)
-
-val s = collection.mutable.Set(1,2,3)
-s += 4
-
-val ims = collection.mutable.Set(2,3,4)
-ims += 5
-ims.add(6)
-ims
-
-ims.max
-ims.min
-
-val mylist = List(1,2,3,1,2,3)
-mylist.toSet
-
-val newset = mylist.toSet
-newset
-
-//Maps key value pair storage
-
-val mymap = Map(("saludo", "Hola"), ("pi", 3.1416), ("z", 1.3))
-mymap("pi")
-mymap("saludo")
-mymap("ja")
-mymap get "pi"
-mymap get "z"
-mymap get "o"
-
-val mutmap = collection.mutable.Map(("z", 123), ("a", 5), ("b", 7))
-
-mutmap += ("lucky" -> 777)
-mutmap
-mutmap.keys
-mutmap.values
-
-//Practice 2
-// 1. Crea una lista llamada "lista" con los elementos "rojo", "blanco", "negro"
-// 2. Añadir 5 elementos mas a "lista" "verde" ,"amarillo", "azul", "naranja", "perla"
-// 3. Traer los elementos de "lista" "verde", "amarillo", "azul"
+//Terminal
+List(verde, amarillo, azul)
 
 //Carlos Garcia 
 
