b05d8e330312a411c54a937b8dfbd6fd6eba95c9
Carlos Garcia <carlos.garcia.valles@uabc.edu.mx>
2025-02-26 20:24:57 -0800

    Ejercicios resueltos Carlos Garcia 02262025

--- BigData/Scala_Basics/Session_03.scala
+++ BigData/Scala_Basics/Session_03.scala
@@ -54,9 +54,84 @@ mutmap.values
 // 1. Crea una lista llamada "lista" con los elementos "rojo", "blanco", "negro"
 // 2. Añadir 5 elementos mas a "lista" "verde" ,"amarillo", "azul", "naranja", "perla"
 // 3. Traer los elementos de "lista" "verde", "amarillo", "azul"
+
+//Carlos Garcia 
+
 // 4. Crea un arreglo de numero en rango del 1-1000 en pasos de 5 en 5
+val arreglo = (1 to 1000 by 5).toArray
+
+//Terminal
+scala>val arreglo = (1 to 1000 by 5).toArray
+val arreglo: Array[Int] = Array(1, 6, 11, 16, 21, 26, 31, 36, 41, 46, 51, 56, 61, 66, 71, 76, 81, 86, 91, 96, 101, 
+106, 111, 116, 121, 126, 131, 136, 141, 146, 151, 156, 161, 166, 171, 176, 181, 186, 191, 196, 201, 206, 211, 216, 
+221, 226, 231, 236, 241, 246, 251, 256, 261, 266, 271, 276, 281, 286, 291, 296, 301, 306, 311, 316, 321, 326, 331, 
+336, 341, 346, 351, 356, 361, 366, 371, 376, 381, 386, 391, 396, 401, 406, 411, 416, 421, 426, 431, 436, 441, 446, 
+451, 456, 461, 466, 471, 476, 481, 486, 491, 496, 501, 506, 511, 516, 521, 526, 531, 536, 541, 546, 551, 556, 561, 
+566, 571, 576, 581, 586, 591, 596, 601, 606, 611, 616, 621, 626, 631, 636, 641, 646, 651, 656, 661, 666, 671, 676, 
+681, 686, 691, 696, 701, 706, 711, 716, 721, 726, 731, 736, 741, 746, 751, 756, 761, 766, 771, 776, 781, 786, 791, 
+796, 801, 806, 811, 816, 821, 826, 831, 836, 841, 846, 851, 856, 861, 866, 871, 876, 881, 886, 891, 896, 901, 906, 
+911, 916, 921, 926, 931, 936, 941, 946, 951, 956, 961, 966, 971, 976, 981, 986, 991, 996)
+
+
+
 // 5. Cuales son los elementos unicos de la lista Lista(1,3,3,4,6,7,3,7) utilice conversion a conjuntos
+
+val lista = List(1, 3, 3, 4, 6, 7, 3, 7)
+val elementosUnicos = lista.toSet
+
+//Terminal
+scala> val lista = List(1, 3, 3, 4, 6, 7, 3, 7)
+val lista: List[Int] = List(1, 3, 3, 4, 6, 7, 3, 7)
+
+scala> val elementosUnicos = lista.toSet
+val elementosUnicos: scala.collection.immutable.Set[Int] = Set(1, 6, 3, 7, 4)
+
+List(1, 3, 4, 6, 7)
+
 // 6. Crea una mapa mutable llamado nombres que contenga los siguiente
 //     "Jose", 20, "Luis", 24, "Ana", 23, "Susana", "27"
+
+import scala.collection.mutable.Map
+
+val nombres = Map(
+  "Jose" -> 20,
+  "Luis" -> 24,
+  "Ana" -> 23,
+  "Susana" -> 27
+)
+
+//Terminal
+scala> val nombres = Map(
+     |   "Jose" -> 20,
+     |   "Luis" -> 24,
+     |   "Ana" -> 23,
+     |   "Susana" -> 27
+     | )
+val nombres: scala.collection.mutable.Map[String,Int] = HashMap(Susana -> 27, Jose -> 20, Ana -> 23, Luis -> 24)
+
+
 // 6 a . Imprime todas la llaves del mapa
-// 7 b . Agrega el siguiente valor al mapa("Miguel", 23)
+
+nombres.keys.foreach(println)
+
+//Terminal
+scala> nombres.keys.foreach(println)
+Susana
+Jose
+Ana
+Luis
+
+// 6 b . Agrega el siguiente valor al mapa("Miguel", 23)
+
+nombres += ("Miguel" -> 23)
+//Terminal
+
+scala> nombres += ("Miguel" -> 23)
+val res2: nombres.type = HashMap(Susana -> 27, Miguel -> 23, Jose -> 20, Ana -> 23, Luis -> 24)
+
+scala> nombres.keys.foreach(println)
+Susana
+Miguel
+Jose
+Ana
+Luis
--- BigData/Scala_Basics/Session_06.scala
+++ BigData/Scala_Basics/Session_06.scala
@@ -5,7 +5,7 @@
 // println(isEven(6))
 // println(isEven(3))
 // Practice 3, analyse the following code with your own words
-
+//------------------------------------
 def listEvens(list:List[Int]): String ={
     for(n <- list){
         if(n%2==0){
@@ -21,7 +21,7 @@ val l = List(1,2,3,4,5,6,7,8)
 val l2 = List(4,3,22,55,7,8)
 listEvens(l)
 listEvens(l2)
-
+//------------------------------------
 //3 7 afortunado
 
 def afortunado(list:List[Int]): Int={
@@ -39,6 +39,8 @@ def afortunado(list:List[Int]): Int={
 val af= List(1,7,7)
 println(afortunado(af))
 
+//Carlos Garcia
+//-------------------------------------
 def balance(list:List[Int]): Boolean={
     var primera = 0
     var segunda = 0
@@ -64,6 +66,56 @@ balance(bl)
 balance(bl2)
 balance(bl3)
 
+//Terminal
+
+scala> def balance(list:List[Int]): Boolean={
+     |     var primera = 0
+     |     var segunda = 0
+     | 
+     |     segunda = list.sum
+     | 
+     |     for(i <- Range(0,list.length)){
+     |         primera = primera + list(i)
+     |         segunda = segunda - list(i)
+     | 
+     |         if(primera == segunda){
+     |             return true
+     |         }
+     |     }
+     |     return false 
+     | }
+def balance(list: List[Int]): Boolean
+
+scala> val bl = List(3,2,1)
+val bl: List[Int] = List(3, 2, 1)
+
+scala> val bl2 = List(2,3,3,2)
+val bl2: List[Int] = List(2, 3, 3, 2)
+
+scala> val bl3 = List(10,30,90)
+val bl3: List[Int] = List(10, 30, 90)
+
+scala> balance(bl)
+val res7: Boolean = true
+
+scala> balance(bl2)
+val res8: Boolean = true
+
+scala> balance(bl3)
+val res9: Boolean = false
+
+// Observaciones
+// el codigo verifica el balance de una lista de enteros y regresa un valor boleano,
+// si exite un punto en la lista donde se suma los elementos de la lista hasta ese punto y 
+// se resta los elementos a la izquierda es igual a la suma de los elementos de la derecha
+// para el primer resultado es true ya que 3+2 = 5 y 1 = 1
+// para el segundo resultado es true ya que 2+3 = 5 y 3+2 = 5
+// para el tercer resultado es false ya que 10+30 = 40 y 90 = 90 por lo que no existe un punto de balance en la lista
+
+
+
+
+//--------------------------------------
 def palindromo(palabra:String):Boolean ={
     return (palabra == palabra.reverse)
 }
@@ -75,3 +127,32 @@ val palabra3 = "JUAN"
 println(palindromo(palabra))
 println(palindromo(palabra2))
 println(palindromo(palabra3))
+
+//Terminal
+scala> def palindromo(palabra:String):Boolean ={
+     |     return (palabra == palabra.reverse)
+     | }
+def palindromo(palabra: String): Boolean
+
+scala> val palabra = "OSO"
+val palabra: String = OSO
+
+scala> val palabra2 = "ANNA"
+val palabra2: String = ANNA
+
+scala> val palabra3 = "JUAN"
+val palabra3: String = JUAN
+
+scala> println(palindromo(palabra))
+true
+
+scala> println(palindromo(palabra2))
+true
+
+scala> println(palindromo(palabra3))
+false
+
+//Observaciones
+//lo que hace el codigo es verificar si la cadena de conectores el string es un palindromo
+//es decir si se lee igual de izquierda a derecha que de derecha a izquierda y lo regresa en un booleano
+//si la palabra es igual en lectura en ambas direcciones es palindromo y regresa true si no es palindromo lo regresa en false
